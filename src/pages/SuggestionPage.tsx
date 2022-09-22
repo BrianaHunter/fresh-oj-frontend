3616c23755f524f6fc15059089a16afc50ae80ec
Bianca Gjokaj <bianca.gjokaj@gmail.com>
2022-09-21 22:20:54 -0400

    got our api to show on our suggestion page

--- src/pages/SuggestionPage.tsx
+++ src/pages/SuggestionPage.tsx
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { fetchSuggestion } from "../services/suggestion.service";
 import { Suggestion, SuggestionResults } from "../types/suggestion.types";
+import { Card, Image, Text, Badge, Button, Group, Grid } from "@mantine/core";
+import AppContainer from "../components/AppContainer";
 
 export default function SuggestionPage() {
   const [suggestions, setSuggestions] = useState<Suggestion[]>([]);
@@ -12,31 +14,68 @@ export default function SuggestionPage() {
     });
   }
 
+  useEffect(() => {
+    getAllSuggestResults();
+  }, []);
+
   return (
-    <div>
+    <AppContainer>
       <div>
         <h1>Helpful Tips and Suggestions</h1>
-        <div className="border-2 border-black">
-          <ul>
+        <div>
+          <Grid>
             {suggestions.map((suggestion) => (
-              <li key={suggestion.Title}>
-                <div>
-                  <h2>{suggestion.Title}</h2>
-                </div>
-                <div>
-                  <p>{suggestion.Categories}</p>
-                </div>
-                <img
-                  src={
-                    "https://health.gov/sites/default/files/" +
-                    suggestion.ImageUrl
-                  }
-                />
-              </li>
+              <Grid.Col md={3} lg={4}>
+                <Card shadow="sm" p="lg" radius="md" withBorder>
+                  <Card.Section component="a" href="https://mantine.dev/">
+                    <Image
+                      className=" w-[200px] h-[200px]"
+                      src={suggestion.ImageUrl}
+                      alt="health"
+                    />
+                  </Card.Section>
+
+                  <Group position="apart" mt="md" mb="xs">
+                    <Text weight={500}>Norway Fjord Adventures</Text>
+                    <Badge color="pink" variant="light">
+                      On Sale
+                    </Badge>
+                  </Group>
+
+                  <Text size="sm" color="dimmed">
+                    With Fjord Tours you can explore more of the magical fjord
+                    landscapes with tours and activities on and around the
+                    fjords of Norway
+                  </Text>
+
+                  <Button
+                    variant="light"
+                    color="blue"
+                    fullWidth
+                    mt="md"
+                    radius="md"
+                  >
+                    Learn More
+                  </Button>
+                </Card>
+              </Grid.Col>
+              // <li key={suggestion.Title}>
+              //   <div>
+              //     <h2>{suggestion.Title}</h2>
+              //   </div>
+              //   <div>
+              //     <p>{suggestion.Categories}</p>
+              //   </div>
+              //   <img
+              //     className="h-[300px] w-[300px]"
+              //     src={suggestion.ImageUrl}
+              //   />
+              // </li>
             ))}
-          </ul>
+          </Grid>
         </div>
       </div>
-    </div>
+      {/* </div> */}
+    </AppContainer>
   );
 }
